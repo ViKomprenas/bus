06e8c5e7f175e12127a7e559f1c2c23c049167ce
Viko <viko@vikomprenas.com>
2016-09-20 16:18:48 -0600

    try to launch a program and fail :(

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::env::args;
+use std::process::Command;
+
 fn main() {
-    let argv: String = std::env::args().skip(1)
-        .fold("".to_string(), |all, item| all + " '" + &item + "'");
-    println!("{}", &argv);
+    let cmdpath = args().skip(1).next();
+    let args = args().skip(2);
+
+    let cmd = Command::new(cmdpath);
+    for arg in args {
+        cmd.arg(arg);
+    }
+
+    cmd.status();
 }
