ed262d00321a88c567c7abedec590c00ea98d7e9
Viko <viko@vikomprenas.com>
2016-09-21 14:24:08 -0600

    [unfinished] keep trying to make it compile

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,23 @@ use std::process::Command;
 use std::fs::OpenOptions;
 use std::io::{stdin, Read, Write};
 
+fn error(message: str) -> Box<Fn() -> !> {
+    Box::new(move || {
+        println!("{}", message);
+        std::process::exit(1);
+    })
+}
+
+fn get_tmp_file() -> String {
+    let err = error("couldn't get mktemp output");
+
+    let path = Command::new("mktemp").output().unwrap_or_else(err).stdout;
+    String::from_utf8(path).unwrap_or_else(err)
+}
+
 fn main() {
-    let cmdpath_ihatetheborrowchecker = args().skip(1).next().unwrap();
-    let cmdpath = cmdpath_ihatetheborrowchecker.as_str();
+    let cmdpath = args().skip(1).next().unwrap_or_else(error(
+            "usage: bus <command> [args...]"));
 
     let args = args().skip(2);
 
@@ -14,8 +28,8 @@ fn main() {
         pagercmd.arg(arg);
     }
 
-    let tmppath = String::from_utf8(Command::new("mktemp").output().unwrap().stdout).unwrap();
-    let mut tmpfile = OpenOptions::new().write(true).open(tmppath).unwrap();
+    let mut tmpfile = OpenOptions::new().write(true).open(get_tmp_file())
+        .unwrap_or_else(error("couldn't open temp file"));
     let mut buffer: Vec<u8> = Vec::with_capacity(8192); // Arbitrary 8KiB.
 
     stdin().read_to_end(&mut buffer);
