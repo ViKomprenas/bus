be7f43c6735fb5526711073fa241792fea12cd3a
Viko <viko@vikomprenas.com>
2016-09-20 18:34:49 -0600

    make it run! for real this time

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use std::process::Command;
 
 fn main() {
     let cmdpath = args().skip(1).next();
+    if cmdpath.is_none() {
+        println!("usage: {} <command>", args().next().unwrap());
+        std::process::exit(1);
+    }
+
     let args = args().skip(2);
 
-    let cmd = Command::new(cmdpath);
+    let mut cmd = Command::new(cmdpath.unwrap().as_str());
     for arg in args {
         cmd.arg(arg);
     }
