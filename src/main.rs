5115e120791856a20e84a64e99eaaac3ea593272
Viko <viko@vikomprenas.com>
2016-09-21 12:55:37 -0600

    make it write out the input (not really but so far so good)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use std::env::args;
 use std::process::Command;
+use std::fs::OpenOptions;
+use std::io::{stdin, Read, Write};
 
 fn main() {
-    let cmdpath = args().skip(1).next();
-    if cmdpath.is_none() {
-        println!("usage: {} <command>", args().next().unwrap());
-        std::process::exit(1);
-    }
+    let cmdpath_ihatetheborrowchecker = args().skip(1).next().unwrap();
+    let cmdpath = cmdpath_ihatetheborrowchecker.as_str();
 
     let args = args().skip(2);
 
-    let mut cmd = Command::new(cmdpath.unwrap().as_str());
+    let mut pagercmd = Command::new(cmdpath);
     for arg in args {
-        cmd.arg(arg);
+        pagercmd.arg(arg);
     }
 
-    cmd.status();
+    let tmppath = String::from_utf8(Command::new("mktemp").output().unwrap().stdout).unwrap();
+    let mut tmpfile = OpenOptions::new().write(true).open(tmppath).unwrap();
+    let mut buffer: Vec<u8> = Vec::with_capacity(8192); // Arbitrary 8KiB.
+
+    stdin().read_to_end(&mut buffer);
+    tmpfile.write_all(&buffer);
 }
