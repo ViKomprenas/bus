da81d17a510a3c95310aab3513ee49411df30060
Viko <viko@vikomprenas.com>
2016-09-22 08:58:07 -0600

    it compiles now!

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,21 @@ use std::process::Command;
 use std::fs::OpenOptions;
 use std::io::{stdin, Read, Write};
 
-fn error(message: str) -> Box<Fn() -> !> {
-    Box::new(move || {
-        println!("{}", message);
-        std::process::exit(1);
-    })
+fn error(message: &str) -> ! {
+    write!(std::io::stderr(), "{}", message);
+    std::process::exit(1);
 }
 
 fn get_tmp_file() -> String {
-    let err = error("couldn't get mktemp output");
+    let err = "couldn't get mktemp output";
 
-    let path = Command::new("mktemp").output().unwrap_or_else(err).stdout;
-    String::from_utf8(path).unwrap_or_else(err)
+    let path = Command::new("mktemp").output().unwrap_or_else(|_|error(err)).stdout;
+    String::from_utf8(path).unwrap_or_else(|_|error(err))
 }
 
 fn main() {
-    let cmdpath = args().skip(1).next().unwrap_or_else(error(
-            "usage: bus <command> [args...]"));
+    let cmdpath = args().skip(1).next()
+            .unwrap_or_else(||error("usage: bus <command> [args...]"));
 
     let args = args().skip(2);
 
@@ -29,7 +27,7 @@ fn main() {
     }
 
     let mut tmpfile = OpenOptions::new().write(true).open(get_tmp_file())
-        .unwrap_or_else(error("couldn't open temp file"));
+        .unwrap_or_else(|_|error("couldn't open temp file"));
     let mut buffer: Vec<u8> = Vec::with_capacity(8192); // Arbitrary 8KiB.
 
     stdin().read_to_end(&mut buffer);
